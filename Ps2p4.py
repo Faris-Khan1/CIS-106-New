d3fe5eacf0116bed3f32adbe6324320a2fa85e22
Faris-Khan1 <89435503+Faris-Khan1@users.noreply.github.com>
2025-02-20 03:03:12 -0600

    Delete Ps2p4.py

--- Ps2p4.py
+++
@@ -1,6 +0,0 @@
-lname = input("Please enter last name: ")
-credit = float(input("Please enter credit hours taken: "))
-fee = 100
-tuition = (credit * 250) + fee
-print(str(lname))
-print("Your total tuition fee is $" + str(tuition))
