038292fd1157dfe1e56be74575f57b3b23e4a4c6
Faris-Khan1 <89435503+Faris-Khan1@users.noreply.github.com>
2025-02-20 03:04:39 -0600

    Add files via upload

---
+++ Session2/Ps2p3.py
@@ -0,0 +1,6 @@
+length = float(input("Please enter length of rectangle:"))
+width = float(input("Please enter width of rectangle:"))
+Area = length * width
+Circumference = (length * 2) + (width * 2)
+print("The area of the rectangle is: " + str(Area))
+print("The circumference of the rectangle is: " + str(Circumference))
