2c2eb897ff664f26b017a7986f9456828004d2f8
Faris-Khan1 <89435503+Faris-Khan1@users.noreply.github.com>
2025-02-20 03:03:18 -0600

    Delete ps2p5.py

--- ps2p5.py
+++
@@ -1,6 +0,0 @@
-price = float(input("Please enter price of the item: "))
-disc_perc = float(input("Please enter discount percent in decimal: "))
-disc_amt = price * disc_perc
-disc_price = price - disc_amt
-print("The discounted amount is $ " + str(disc_amt))
-print("The discounted price is $ " + str(disc_price))
