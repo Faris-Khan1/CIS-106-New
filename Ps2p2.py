5636fdc3582e304678936ec8e03bc9b8b9696380
Faris-Khan1 <89435503+Faris-Khan1@users.noreply.github.com>
2025-02-20 03:03:00 -0600

    Delete Ps2p2.py

--- Ps2p2.py
+++
@@ -1,6 +0,0 @@
-lname = input("Please enter last name: ")
-hrs = float(input("Please enter hours worked: "))
-rate = float(input("Please enter payrate: "))
-gross = hrs * rate
-print(str(lname))
-print("Your gross pay is $" + str(gross))
